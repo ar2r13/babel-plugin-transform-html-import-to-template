3aa74a78d40c5885577a263fbf2aff85be2b6a3b
Arthur Lavrischev <ar2r13lawrence@gmail.com>
2020-03-22 07:20:56 +0300

    v 0.1.0

--- src/index.js
+++ src/index.js
@@ -12,34 +12,50 @@ export default ({ types: t }) => ({
     visitor: {
         ImportDefaultSpecifier({ parentPath }, { file }) {
             const { value } = parentPath.node.source
+            const extension = ext(value)
 
-            if (ext(value) != '.html') return
-
+            if (!['.css', '.html'].includes(extension)) return
+            
             const dir = p.dirname(p.resolve(file.opts.filename))
             const absolutePath = p.resolve(dir, value)
-            const html = fs.readFileSync(absolutePath, 'utf8') || ''
+            const content = fs.readFileSync(absolutePath, 'utf8') || ''
 
-            parentPath.replaceWithMultiple([
-                t.variableDeclaration('var', [
+            if (extension === '.html') 
+                parentPath.replaceWithMultiple([
+                    t.variableDeclaration('var', [
+                        t.variableDeclarator(
+                            t.identifier(parentPath.node.specifiers[0].local.name),
+                            t.callExpression(
+                                t.memberExpression(
+                                    t.identifier('document'),
+                                    t.identifier('createElement')
+                                ),
+                                [t.stringLiteral('template')]
+                            )
+                        )]),
+                    t.expressionStatement(t.assignmentExpression(
+                        '=',
+                        t.memberExpression(
+                            t.identifier('template'),
+                            t.identifier('innerHTML')
+                        ),
+                        t.stringLiteral(content)
+                    ))
+                ])
+
+            if (extension === '.css') 
+                parentPath.replaceWith(t.variableDeclaration('var', [
                     t.variableDeclarator(
                         t.identifier(parentPath.node.specifiers[0].local.name),
                         t.callExpression(
                             t.memberExpression(
-                                t.identifier('document'),
-                                t.identifier('createElement')
+                                t.newExpression(t.identifier('CSSStyleSheet'), []),
+                                t.identifier('replaceSync')
                             ),
-                            [t.stringLiteral('template')]
+                            [t.stringLiteral(content)]
                         )
-                    )]),
-                t.expressionStatement(t.assignmentExpression(
-                    '=',
-                    t.memberExpression(
-                        t.identifier('template'),
-                        t.identifier('innerHTML')
-                    ),
-                    t.stringLiteral(html)
-                ))
-            ])
+                    )
+                ]))
         }
     }
 })
